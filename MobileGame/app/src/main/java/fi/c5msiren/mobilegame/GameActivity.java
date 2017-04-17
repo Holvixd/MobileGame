2cadce5d7f1ef3fe88dbeeb537f9d97076a4c885
mssq <smiikasiren@gmail.com>
2017-04-17 12:52:23 +0300

    - Added basic logic for calling the GameView

--- MobileGame/app/src/main/java/fi/c5msiren/mobilegame/GameActivity.java
+++ MobileGame/app/src/main/java/fi/c5msiren/mobilegame/GameActivity.java
@@ -1,13 +1,37 @@
 package fi.c5msiren.mobilegame;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 
 public class GameActivity extends AppCompatActivity {
+
+    // Declaring GameView
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+
+        //Initializing game view object
+        gameView = new GameView(this);
+
+        // Adding it to ContentView
+        setContentView(gameView);
+    }
+
+    // Pausing the game when activity is paused
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    // Running the game when activity is resumed
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
     }
 }
