126c2441595bf2ae8c4fd8d4904eaccc999b9dc5
mssq <smiikasiren@gmail.com>
2017-04-17 16:06:08 +0300

    - Added collision box

--- MobileGame/app/src/main/java/fi/c5msiren/mobilegame/Player.java
+++ MobileGame/app/src/main/java/fi/c5msiren/mobilegame/Player.java
@@ -3,6 +3,7 @@ package fi.c5msiren.mobilegame;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 
 /**
  * Created by Miika on 17.4.2017.
@@ -30,6 +31,8 @@ public class Player {
     private final int MIN_SPEED = 1;
     private final int MAX_SPEED = 20;
 
+    private Rect detectCollision;
+
     public Player(Context context, int screenX, int screenY) {
         x = 75;
         y = 50;
@@ -44,6 +47,9 @@ public class Player {
 
         // Setting the initial value to false
         accelerating = false;
+
+        //initializing rect object
+        detectCollision =  new Rect(x, y, playerBitmap.getWidth(), playerBitmap.getHeight());
     }
 
     public void update()  {
@@ -73,6 +79,16 @@ public class Player {
         if (y > maxY) {
             y = maxY;
         }
+
+        //adding top, left, bottom and right to the rect object
+        detectCollision.left = x;
+        detectCollision.top = y;
+        detectCollision.right = x + playerBitmap.getWidth();
+        detectCollision.bottom = y + playerBitmap.getHeight();
+    }
+
+    public Rect getDetectCollision() {
+        return detectCollision;
     }
 
     public void startAccelerating() {
