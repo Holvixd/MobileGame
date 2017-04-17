32f86cbb91a9a33bb78d90ba2f4e84266527f073
mssq <smiikasiren@gmail.com>
2017-04-17 14:00:44 +0300

    - Added player movement on the Y axis

--- MobileGame/app/src/main/java/fi/c5msiren/mobilegame/Player.java
+++ MobileGame/app/src/main/java/fi/c5msiren/mobilegame/Player.java
@@ -12,25 +12,75 @@ public class Player {
 
     // Character image
     private Bitmap playerBitmap;
-
     // Coordinates
     private int x, y;
-
     // Player speed
     private int speed = 0;
 
-    public Player(Context context) {
+    // Track if the user presses the screen
+    private boolean accelerating;
+    // Acceleration speed
+    private int accelerationSpeed = 5;
+    // Gravity value to add gravity effect
+    private int gravity = -10;
+
+    // Boundaries so player doesn't go outside the screen
+    private int maxY, minY;
+    // Boundaries to player speed
+    private final int MIN_SPEED = 1;
+    private final int MAX_SPEED = 20;
+
+    public Player(Context context, int screenX, int screenY) {
         x = 75;
         y = 50;
         speed = 1;
-
         // Setting the player image
         playerBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.player);
+
+        // Calculating maxY
+        maxY = screenY - playerBitmap.getHeight();
+        // Top edge's y point is always zero
+        minY = 0;
+
+        // Setting the initial value to false
+        accelerating = false;
     }
 
     public void update()  {
-        // Update x coordinate
-        x++;
+        // If player is accelerating
+        if (accelerating) {
+            speed += accelerationSpeed;
+        } else {
+            speed -= 5;
+        }
+
+        // Boundary for maximum speed
+        if (speed > MAX_SPEED) {
+            speed = MAX_SPEED;
+        }
+        // Boundary for minimum speed so the player wont stop
+        if (speed < MIN_SPEED) {
+            speed = MIN_SPEED;
+        }
+
+        // Move player downwards
+        y -= speed + gravity;
+
+        // Boundary for player so he won't go off the screen
+        if (y < minY) {
+            y = minY;
+        }
+        if (y > maxY) {
+            y = maxY;
+        }
+    }
+
+    public void startAccelerating() {
+        accelerating = true;
+    }
+
+    public void stopAccelerating() {
+        accelerating = false;
     }
 
     public Bitmap getPlayerBitmap() {
