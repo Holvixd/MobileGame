58067ba7f18e6e717f29dd68aa3a03aedf729067
mssq <smiikasiren@gmail.com>
2017-04-30 15:03:50 +0300

    Activity for displaying the highscores. Current version has dummydata and data is not yet fetched from the database.

--- MobileGame/app/src/main/java/fi/c5msiren/mobilegame/HighscoreActivity.java
+++ MobileGame/app/src/main/java/fi/c5msiren/mobilegame/HighscoreActivity.java
@@ -2,12 +2,44 @@ package fi.c5msiren.mobilegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class HighscoreActivity extends AppCompatActivity {
+
+    ListView list;
+    String[] web = new String[] { "Android", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7"};
+    Integer[] imageId = {
+            R.drawable.one,
+            R.drawable.two,
+            R.drawable.three,
+            R.drawable.four,
+            R.drawable.five,
+            R.drawable.six,
+            R.drawable.seven
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
+
+        HighscoreArrayAdapter adapter = new
+                HighscoreArrayAdapter(HighscoreActivity.this, web, imageId);
+        list = (ListView)findViewById(R.id.listview);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(HighscoreActivity.this, "Player: " +web[+ position], Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
