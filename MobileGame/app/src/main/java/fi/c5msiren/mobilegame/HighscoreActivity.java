4c57db36e6a0a61c3a2146c3ea966df63eda1776
mssq <smiikasiren@gmail.com>
2017-04-30 12:58:59 +0300

    Created the base activity for showing the highscores

---
+++ MobileGame/app/src/main/java/fi/c5msiren/mobilegame/HighscoreActivity.java
@@ -0,0 +1,13 @@
+package fi.c5msiren.mobilegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighscoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+    }
+}
