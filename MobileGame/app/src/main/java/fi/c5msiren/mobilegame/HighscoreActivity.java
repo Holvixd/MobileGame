25e0af55f461be3ed4239a7d4ea71110094ae0ec
mssq <smiikasiren@gmail.com>
2017-05-01 15:03:14 +0300

    - Added connecting to the backend - Added fetching jsonData from the backend - Modified to display fetched data instead of dummydata

--- MobileGame/app/src/main/java/fi/c5msiren/mobilegame/HighscoreActivity.java
+++ MobileGame/app/src/main/java/fi/c5msiren/mobilegame/HighscoreActivity.java
@@ -1,5 +1,8 @@
 package fi.c5msiren.mobilegame;
 
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,40 +10,136 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+
 public class HighscoreActivity extends AppCompatActivity {
 
+    // ListView for showing all the scores
     ListView list;
-    String[] name = new String[] { "Android", "iPhone", "WindowsMobile",
-            "Blackberry", "WebOS", "Ubuntu", "Windows7"};
-    int[] score = new int[] {350, 252, 235, 730, 134, 525, 623};
-    Integer[] imageId = {
-            R.drawable.one,
-            R.drawable.two,
-            R.drawable.three,
-            R.drawable.four,
-            R.drawable.five,
-            R.drawable.six,
-            R.drawable.seven
-    };
+    // JSONArray for storing the data fetched from backend
+    JSONArray jsonScores = new JSONArray();
+    // String array for storing the name of players
+    String[] name;
+    // Integer array for storing the player scores
+    int[] score;
+    // Integer array for storing the images
+    Integer[] imageId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
 
-        HighscoreArrayAdapter adapter = new
-                HighscoreArrayAdapter(HighscoreActivity.this, name, score, imageId);
-        list = (ListView)findViewById(R.id.listview);
-        list.setAdapter(adapter);
+        // Start AsynchronousTask getScores
+        new getScores().execute("");
+    }
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    public class getScores extends AsyncTask<String, String, String> {
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Toast.makeText(HighscoreActivity.this, "Player: " + name[+ position] + " Score: " + score[+ position], Toast.LENGTH_SHORT).show();
+        @Override
+        protected String doInBackground(String... args) {
 
+            try {
+                // Connect to the backend
+                URL url = new URL("http://10.0.2.2:8080/scores");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                InputStream in = conn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                // Get the json string from the backend and add it to JSONArray
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    jsonScores = new JSONArray(line);
+                }
+
+                // The list only shows the best 9 scores, if it fetches more only display nine.
+                if (jsonScores.length() > 9) {
+                    score = new int[9];
+                    name = new String[9];
+                    imageId = new Integer[9];
+                    // Loop through the JSONArray and get the data from individual
+                    // JSONObjects and store it in the arrays
+                    for (int i = 0; i < 9; i++) {
+                        JSONObject scoreObject = new JSONObject(jsonScores.get(i).toString());
+                        score[i] = scoreObject.getInt("score");
+                        name[i] = scoreObject.getString("name");
+                        imageId[i] = getResources().getIdentifier("image_" + (i+1), "drawable", "fi.c5msiren.mobilegame");
+                        System.out.println("SCOREOBJECT: " + scoreObject);
+                    }
+                } else {
+                    // Make arrays length based what you got from the backend
+                    score = new int[jsonScores.length()];
+                    name = new String[jsonScores.length()];
+                    imageId = new Integer[jsonScores.length()];
+                    // Loop through the JSONArray and get the data from individual
+                    // JSONObjects and store it in the arrays
+                    for (int i = 0; i < jsonScores.length(); i++) {
+                        JSONObject scoreObject = new JSONObject(jsonScores.get(i).toString());
+                        score[i] = scoreObject.getInt("score");
+                        name[i] = scoreObject.getString("name");
+                        imageId[i] = getResources().getIdentifier("image_" + (i+1), "drawable", "fi.c5msiren.mobilegame");
+                        System.out.println("SCOREOBJECT: " + scoreObject);
+                    }
+                }
+
+
+                // Close bufferedreader, inputstream and connection.
+                reader.close();
+                in.close();
+                conn.disconnect();
+
+            // If cant connect to backend server, show error message
+            } catch(MalformedURLException | SocketTimeoutException | ProtocolException e) {
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Server error occurred",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                e.printStackTrace();
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
             }
-        });
+
+            return "Done";
+        }
+
+        // After fetching the data show it on the adapter
+        @Override
+        protected void onPostExecute(String result) {
+
+            // Create new adapter with the fetched data
+            HighscoreArrayAdapter adapter = new
+                    HighscoreArrayAdapter(HighscoreActivity.this, name, score, imageId);
+            list = (ListView)findViewById(R.id.listview);
+            list.setAdapter(adapter);
+
+            // On click show the score information
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    Toast.makeText(HighscoreActivity.this, "Player: " + name[+ position] + " Score: " + score[+ position], Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+
     }
 }
