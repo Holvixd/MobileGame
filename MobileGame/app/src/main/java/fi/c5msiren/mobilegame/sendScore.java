6d421d4d212d8a875aa5c0e5b06df5b2a7c6623a
mssq <smiikasiren@gmail.com>
2017-05-05 19:35:50 +0300

    Connects to the back end and sends the player score as JSON

---
+++ MobileGame/app/src/main/java/fi/c5msiren/mobilegame/sendScore.java
@@ -0,0 +1,101 @@
+package fi.c5msiren.mobilegame;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+
+/**
+ * Created by Miika on 5.5.2017.
+ */
+
+public class sendScore extends AsyncTask<String, String, String> {
+
+    // Reference to the activity that calls this class
+    private Activity activity;
+    // Reference to the player score
+    private Score score;
+
+    public sendScore(Activity activity, Score score) {
+        System.out.println("THIS GETS CALLED!!");
+        this.activity = activity;
+        this.score = score;
+    }
+
+    @Override
+    protected String doInBackground(String... args) {
+
+        try {
+            // Connect to the back end
+            URL url = new URL("http://10.0.2.2:8080/scores");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setConnectTimeout(5000);
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.connect();
+
+            // Add the score information to JSON object
+            JSONObject jsonParam = new JSONObject();
+            jsonParam.put("name", score.getName());
+            jsonParam.put("score", score.getAmount());
+
+            // Send the JSON data to the server
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+            bufferedWriter.write(jsonParam.toString());
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            os.close();
+
+            InputStream inputStream = conn.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+
+            bufferedReader.close();
+            inputStream.close();
+            conn.disconnect();
+
+        } catch (MalformedURLException | SocketTimeoutException | ProtocolException e) {
+            // If cant connect to backend server, show error message
+            // Connecting to server failed
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(activity.getApplicationContext(),
+                            "Server error occurred",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        return "Done";
+    }
+
+    // After fetching the data show it on the adapter
+    @Override
+    protected void onPostExecute(String result) {
+        // Reset score
+        score.setScore(0);
+    }
+}
