1a3c284e24a76a040118e9d40ed82a7ca08958cd
mssq <smiikasiren@gmail.com>
2017-05-06 12:41:27 +0300

    Added pop-up textbox where player enters his name

--- MobileGame/app/src/main/java/fi/c5msiren/mobilegame/GameActivity.java
+++ MobileGame/app/src/main/java/fi/c5msiren/mobilegame/GameActivity.java
@@ -1,45 +1,62 @@
 package fi.c5msiren.mobilegame;
 
+import android.content.DialogInterface;
 import android.graphics.Point;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Display;
+import android.widget.EditText;
 
 public class GameActivity extends AppCompatActivity {
 
     // Declaring GameView
     private GameView gameView;
 
+    private String playerName = "";
+
+    private Point size = new Point();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set content view temporarily to activity_game
+        setContentView(R.layout.activity_game);
 
         // Getting display object
         Display display = getWindowManager().getDefaultDisplay();
 
         // Get screen resolution
-        Point size = new Point();
         display.getSize(size);
 
-        // Initializing game view object
-        gameView = new GameView(this, size.x, size.y);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Player name");
 
-        // Adding it to ContentView
-        setContentView(gameView);
-    }
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
 
-    // Pausing the game when activity is paused
-    @Override
-    protected void onPause() {
-        super.onPause();
-        gameView.pause();
-    }
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                playerName = input.getText().toString();
 
-    // Running the game when activity is resumed
-    @Override
-    protected void onResume() {
-        super.onResume();
-        gameView.resume();
+                // Initializing game view object
+                gameView = new GameView(GameActivity.this, size.x, size.y, playerName);
+                // Adding it to ContentView
+                setContentView(gameView);
+                // And starting it
+                gameView.resume();
+            }
+        });
+
+        builder.show();
     }
 
 }
